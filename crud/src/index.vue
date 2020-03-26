30ccf897ebb47a64ab3b93ec0cd791c3cedbf91b
yubaobin <502275531@qq.com>
2020-03-26 19:03:55 +0800

    add template

--- crud/src/index.vue
+++ crud/src/index.vue
@@ -60,9 +60,9 @@ export default {
   },
   data () {
     return {
-      tableUrl: this.api.{{ tableUrl }},
-      deleteUrl: this.api.{{ deleteUrl }},
-      deleteBatchUrl: this.api.{{ deleteBatchUrl }},
+      tableUrl: this.api.tableUrl,
+      deleteUrl: this.api.deleteUrl,
+      deleteBatchUrl: this.api.deleteBatchUrl,
       query: {
         productVo: {
           name: ''
